e7569996e19fe13d5a7f6bbf6c6f443aff96ee61
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-02-07 10:37:19 +0000

    add tests and code for roll_some_dice

--- lib/dice.rb
+++ lib/dice.rb
@@ -2,4 +2,10 @@ class Dice
   def roll
     rand(6) + 1
   end
+
+  def roll_some_dice(quantity)
+    results = []
+    quantity.times {results.push(roll)}
+    return results
+  end
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,14 +2,25 @@ require 'dice'
 
 describe Dice do
   it {is_expected.to respond_to :roll}
+
   it 'can return a number between 1 and 6 when rolled' do
     expect(subject.roll).to be_between(1,6).inclusive
   end
+
   it 'can return a random number between 1 and 6 when rolled' do
     arr = []
     10.times do
       arr.push(subject.roll)
     end
     expect(arr.uniq.length).to be > 1
+  end
+
+  it {is_expected.to respond_to :roll_some_dice}
+
+  it 'can return a set of results from several rolls' do
+    expect(subject.roll_some_dice(2).length).to eq(2)
+    expect(subject.roll_some_dice(10).length).to eq(10)
+    expect(subject.roll_some_dice(0).length).to eq(0)
+    expect(subject.roll_some_dice(1).length).to eq(1)
   end
 end
